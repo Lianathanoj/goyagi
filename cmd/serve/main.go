ad84470e50e2ee6fe0155e68cf942fa7bacbaee6
Jonathan Lian <jlian@gatech.edu>
2019-06-25 10:26:57 -0700

    feat(application): create shared app (#3)

--- cmd/serve/main.go
+++ cmd/serve/main.go
@@ -4,16 +4,18 @@ package main
 import (
     "net/http"
 
+    "github.com/Lianathanoj/goyagi/pkg/application"
     "github.com/Lianathanoj/goyagi/pkg/server"
     "github.com/lob/logger-go"
 )
 
 func main() {
     log := logger.New()
+    app := application.New()
 
-    srv := server.New()
+    srv := server.New(app)
 
-    log.Info("server started")
+    log.Info("server started", logger.Data{"port": app.Config.Port})
 
     err := srv.ListenAndServe()
     if err != nil && err != http.ErrServerClosed {
